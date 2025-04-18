1c0ecbfecb2cc6387a55b3cf957c8f8f74ea4015
Asifsaifali <asif263848@gmail.com>
2025-04-18 23:03:58 +0530

    Initializing the project_1

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello boy")
+})
+
+app.listen(8000, ()=>{
+    console.log("POrt at 8000");
+    
+})
