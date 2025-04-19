432cf7db7819a4a251af5cbc214fdc67f063af07
Asifsaifali <asif263848@gmail.com>
2025-04-19 14:56:20 +0530

    Added user.model file in project

---
+++ model/User.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String,
+    role : {
+        type : String,
+        enum : ["user", "admin"],
+        default : "user"
+    },
+    VerificationToken: {
+        type : String,
+    },
+    resetPasswordExpiry: {
+        type: Date,
+    }
+
+},{
+    timestamps: true
+})
+
+
+const User = mongoose.model("User", userSchema)
+
+export default User
